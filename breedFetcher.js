c1a8b0cc334a7bbfd4a067ec3e4df00313c6a87f
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-05-06 23:34:40 +0000

    Refractored breedFetcher and split into 2 files. Added testing function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,30 +1,25 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
-
-let catQuery = args[0];
-
-const breedFetcher = () => {
-
-
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${catQuery}`, (error, response, body) => {
+const fetchBreedDescription = (breedName, callback) => {
+  
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    
+    if (error) {
+      return callback(error, null);
+    }
     
     const data = JSON.parse(body);
-    
-    if (data.message) {
-      console.log(data.message);
-    }
-
 
     if (data[0] === undefined) {
-      console.log('THATS NOT A KITTY,TRY AGAIN');
+      return callback('THATS NOT A KITTY', null);
     } else {
-      console.log(data[0].description);
+      return callback(null, data[0].description);
     }
-
   });
 };
 
-breedFetcher();
 
+module.exports = {
+  fetchBreedDescription
+};
 
---
+++ test/breedFetcherTest.js
@@ -0,0 +1,26 @@
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDesscription', () => {
+
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+
+    
+  });
+  it('return false or something if invalid breed or doesnt exist', (done) => {
+    fetchBreedDescription('LadyBird', (err) => {
+
+      assert.equal(err, 'THATS NOT A KITTY');
+
+      done();
+    });
+  });
+});
